bb2a8023410245065cdfac5463766eb71f10d7ed
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-05-19 11:54:02 +0300

    исправил фильтр и добавил сортировку по пунктам
    стал заполнять базу и увидел что сортировки не достаточно,
    а фильтрация не по тому столбцу. исправил. заполняю дальше.

--- the_menu_app/admin.py
+++ the_menu_app/admin.py
@@ -6,8 +6,8 @@ from .models import MenuName, MenuPoint
 @admin.register(MenuPoint)
 class MenuPointAdmin(admin.ModelAdmin):
     list_display = ['point', 'above', 'menu']
-    ordering = ['menu', 'above']
-    list_filter = ['point']
+    ordering = ['menu', 'above', 'point']
+    list_filter = ['above']
 
 
 @admin.register(MenuName)
