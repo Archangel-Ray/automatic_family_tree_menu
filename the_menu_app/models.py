792f7f6fe193600331d66b269a5fe7df8c7d00f3
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-05-19 11:03:33 +0300

    создал модели
    это самое простое. я уже два раза их прописывал. но каждый раз старался
    как-то улучшить. в этот раз тоже по новой. во-первых, вынес название
    меню в отдельную таблицу, чтоб иметь возможность безболезненно менять
    ему название. потом, отказался от слаг полей, потому что я так их и не
    использовал. ну, а основная идея конечно сохранилась - рекурсивное
    подключение таблицы себя хорошо оправдало.
    пока ошибок не вижу, сейчас проведу миграции,
    если что-то вылезет, то поправлю.

--- the_menu_app/models.py
+++ the_menu_app/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class MenuName(models.Model):
+    name = models.CharField(unique=True)
+
+    class Meta:
+        verbose_name = 'Меню'
+        verbose_name_plural = 'Меню'
+
+    def __str__(self):
+        return self.name
+
+
+class MenuPoint(models.Model):
+    point = models.CharField()
+    menu = models.ForeignKey(MenuName, blank=True, related_name='points', on_delete=models.CASCADE)
+    above = models.ForeignKey('self', blank=True, null=True, related_name='submenu', on_delete=models.SET_NULL)
+
+    class Meta:
+        verbose_name = 'пункт'
+        verbose_name_plural = 'пункты'
+
+    def __str__(self):
+        return self.point
