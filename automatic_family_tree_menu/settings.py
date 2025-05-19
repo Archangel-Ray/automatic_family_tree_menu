ff7d7f55954cbaa15e16b4c72b3dfd4a7cfb06d8
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-05-19 13:31:02 +0300

    базовый шаблон
    создал базовый шаблон. пока только самое необходимое.
    потом если что-то понадобиться, то добавиться.
    и подключил шаблоны на уровне проекта.
    ________________________________________________________________________
    заполнил базу аж на сто двадцать пунктов меню.
    чтоб было на чём проверять работу тега.
    немного заморочился, но надеюсь это будет полезно.
    и на этом, наверно, сегодня всё. надо переключаться на работу.
    не получиться сегодня увидеть что-то готовое, завтра продолжу.

--- automatic_family_tree_menu/settings.py
+++ automatic_family_tree_menu/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'automatic_family_tree_menu.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR/'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
