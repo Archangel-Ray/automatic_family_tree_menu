49cd8cd21a49eb0624c1f737710ff25a2e56ad26
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-05-20 22:12:35 +0300

    закончил логику тега
    УРА!!! у меня это получилось! всё работает как сказано в задании.
    и не совсем то я был в тупике. дополнительные пара часов проб и ошибок
    и всё получилось. а значит идея была правильной.
    мне немного не нравится сам код. так что я его немного подшлифую,
    сделаю пояснения, потому-что самому сложно разобраться что здесь
    к чему и куда. потом оформлю РидМи и можно будет отправлять на проверку.

--- the_menu_app/templatetags/tags_for_menu.py
+++ the_menu_app/templatetags/tags_for_menu.py
@@ -8,9 +8,24 @@ register = template.Library()
 @register.inclusion_tag('the_menu_app/this_is_the_menu.html', takes_context=True)
 def draw_menu(context, menu_name):
     current_menu = MenuName.objects.get(name=menu_name).points.all()
+    upper_level = [{'current_point': x} for x in list(current_menu.filter(above=None))]
     family_tree = []
     if menu_name in context.request.GET:
-        family_tree.append(current_menu.get(id=context.request.GET[menu_name]))
-    while family_tree[0].above:
-        family_tree.insert(0, family_tree[0].above)
-    return {'menu': family_tree}
+        current_point = current_menu.get(id=context.request.GET[menu_name])
+        submenu = [{'current_point': x} for x in list(current_menu.filter(above=current_point))]
+        family_tree.append({'current_point': current_point, 'submenu': submenu})
+
+        while family_tree[0]['current_point'].above:
+            current_point = family_tree[0]['current_point'].above
+            submenu = [{'current_point': x} for x in list(current_menu.filter(above=current_point))]
+            for current_level_point in submenu:
+                if current_level_point['current_point'] == family_tree[0]['current_point']:
+                    current_level_point['submenu'] = family_tree[0]['submenu']
+            family_tree.clear()
+            family_tree.append({'current_point': current_point, 'submenu': submenu})
+
+        for upper_level_point in upper_level:
+            if upper_level_point['current_point'] == family_tree[0]['current_point']:
+                upper_level_point['submenu'] = family_tree[0]['submenu']
+
+    return {'menu': upper_level}
