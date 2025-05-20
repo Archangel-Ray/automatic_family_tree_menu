ba3140a54267b981abd6d4659b1b541d6f44c569
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-05-20 15:08:11 +0300

    выстроил основное дерево
    надо сохраниться.
    постоянно приходится отвлекаться от задания на работу.
    надеялся, что сегодня уже получится отправить готовое задание,
    но похоже буду доделывать ещё завтра.
    мечтаю об одном: чтоб ничего не отвлекало от кода.
    
    и так, что получилось на этот момент:
    - переписал получение всех пунктов меню более эстетичным способом
    - получил выбранный пункт
    - выстроил ствол дерева меню от выбранного пункта до верхнего уровня
    теперь надо вывести подуровни. ещё не придумал как.

--- the_menu_app/templatetags/tags_for_menu.py
+++ the_menu_app/templatetags/tags_for_menu.py
@@ -1,11 +1,16 @@
 from django import template
 
-from ..models import MenuPoint
+from ..models import MenuPoint, MenuName
 
 register = template.Library()
 
 
 @register.inclusion_tag('the_menu_app/this_is_the_menu.html', takes_context=True)
 def draw_menu(context, menu_name):
-    current_menu = {'menu': MenuPoint.objects.filter(menu__name=menu_name)}
-    return current_menu
+    current_menu = MenuName.objects.get(name=menu_name).points.all()
+    family_tree = []
+    if menu_name in context.request.GET:
+        family_tree.append(current_menu.get(id=context.request.GET[menu_name]))
+    while family_tree[0].above:
+        family_tree.insert(0, family_tree[0].above)
+    return {'menu': family_tree}
