97fd15e5adec0472f30afae620d246f9af84ab6d
Сергей Савельев <sergey-savelyev-2024@yandex.ru>
2025-05-20 17:23:21 +0300

    добавил списки подменю и снова в тупике
    я снова зашёл в тупик. что у меня получилось в этот раз:
    решил в список ствола меню добавить остальные пункты текущего уровня.
    для этого превратил пункт в словарь и добавил ему его подпункты. но это
    никак не помогло. во-первых отрезается самый верхний уровень.
    во-вторых, не понятно, как выводить остальные пункты этого уровня
    совсем не понятно. начинать всё в четвёртый раз совсем нет желания.
    по этому откатываюсь назад и продолжу на другой ветке.
    и вообще, на каждое направление (по идее) надо делать отдельную ветку.
    за одно все пробы будут сохраняться в одном репозитории и можно будет
    посмотреть что уже пробовалось и не повторять пройденных ошибок.

--- the_menu_app/templatetags/tags_for_menu.py
+++ the_menu_app/templatetags/tags_for_menu.py
@@ -8,9 +8,16 @@ register = template.Library()
 @register.inclusion_tag('the_menu_app/this_is_the_menu.html', takes_context=True)
 def draw_menu(context, menu_name):
     current_menu = MenuName.objects.get(name=menu_name).points.all()
+
     family_tree = []
     if menu_name in context.request.GET:
-        family_tree.append(current_menu.get(id=context.request.GET[menu_name]))
-    while family_tree[0].above:
-        family_tree.insert(0, family_tree[0].above)
+        current_point = current_menu.get(id=context.request.GET[menu_name])
+        submenu = current_menu.filter(above=current_point)
+        family_tree.append({'current_point': current_point, 'submenu': submenu})
+
+    while family_tree[0]['current_point'].above:
+        current_point = family_tree[0]['current_point'].above
+        submenu = current_menu.filter(above=current_point.above)
+        family_tree.insert(0, {'current_point': current_point, 'submenu': submenu})
+
     return {'menu': family_tree}
